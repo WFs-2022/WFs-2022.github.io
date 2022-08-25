0a6b1dab098b1995a49e101043d19deab8bdbcc1
W=F*s <106474136+WFs-2022@users.noreply.github.com>
2022-08-25 21:19:56 +0800

    更新 起始页

--- startpage.js
+++ startpage.js
@@ -7,16 +7,21 @@ var searchEngine = 0;
 var searching = false, searchInPage = false;
 srchBox.onfocus=()=>{
     searching = true;
+    document.body.style.animationName="backblur"
 }
 srchBox.onblur=()=>{
     srchBox.classList.add("srchBoxlosfoc")
     searching = false;
+    document.body.style.animationName="debackblur"
 }
 document.onkeydown=function(event){
     var e = event || window.event || arguments.callee.caller.arguments[0];
     if(e && e.keyCode==13 && searching){
         if(!searchInPage) open(searchEngines[0]+srchBox.value);
         else window.location=(searchEngines[0]+srchBox.value);
+    }else if(e.keyCode==191){
+        e.preventDefault();
+        srchBox.focus();
     }
 };
 

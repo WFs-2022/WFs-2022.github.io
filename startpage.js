ce7edc920aaed671a36146cb62837b983abf6f83
W=F*s <106474136+WFs-2022@users.noreply.github.com>
2022-08-25 22:59:30 +0800

    更新 起始页

--- startpage.js
+++ startpage.js
@@ -1,9 +1,31 @@
+window.onload=()=>{
+    showTime();setInterval(()=>{showTime()},500);
+}
+let timeshow = document.getElementById("time")
+let dateshow = document.getElementById("date")
+function showTime(){
+    var day = new Date();
+    var hour = day.getHours();
+    var minut = day.getMinutes().toString();
+    var secon = day.getSeconds().toString();
+    var year = day.getFullYear();
+    var month = day.getMonth();
+    var date = day.getDate();
+    if(minut.length==1) minut="0"+minut;
+    if(secon.length==1) secon="0"+secon;
+    timeshow.innerText = hour+":"+minut+":"+secon;
+    dateshow.innerText = year+"年"+month+"月"+date+"日";
+}
+
 let srchBox = document.getElementById("srchBox")
+let setbtn  = document.getElementById("setting")
+let setpage = document.getElementById("settingpage")
 var searchEngines = new Array(
-    "https://www.baidu.com/s?wd=",
-    "https://www.sogou.com/web?query="
+    "https://www.baidu.com/s?wd=",    // 百度
+    "https://www.sogou.com/web?query="// 搜狗
 )
-var searchEngine = 0;
+var searchEngine = 0
+var setpagerun = false, setpageon = false
 var searching = false, searchInPage = false;
 srchBox.onfocus=()=>{
     searching = true;
@@ -22,24 +44,34 @@ document.onkeydown=function(event){
     }else if(e.keyCode==191){
         e.preventDefault();
         srchBox.focus();
+    }else if(e.keyCode==191){
+        e.preventDefault();
+        srchBox.focus();
+    }else if(e.keyCode==27){
+        srchBox.blur();
     }
+    // else{
+    //     alert(e.keyCode);
+    // }
 };
-
-let timeshow = document.getElementById("time")
-let dateshow = document.getElementById("date")
-function showTime(){
-    var day = new Date();
-    var hour = day.getHours();
-    var minut = day.getMinutes().toString();
-    var secon = day.getSeconds().toString();
-    var year = day.getFullYear();
-    var month = day.getMonth();
-    var date = day.getDate();
-    if(minut.length==1) minut="0"+minut;
-    if(secon.length==1) secon="0"+secon;
-    timeshow.innerText = hour+":"+minut+":"+secon;
-    dateshow.innerText = year+"年"+month+"月"+date+"日";
+document.oncontextmenu=function(event){
+    var e = event || window.event;
+    e.preventDefault();
 }
-window.onload=()=>{
-    showTime();setInterval(()=>{showTime()},500);
+setbtn.onclick=()=>{
+    if(setpageon){
+        setpageon = false
+        setpage.style.animation="setpagecls 1s forwards"
+    }else{
+        setpageon = true
+        setpage.style.animation="setpageact 1s forwards"
+    }
+}
+function showAbout() {
+    document.getElementById('about').style.transform='translate(-50%, -50%)';
+    document.getElementById("about").style.transitionDuration="0.5s"
+}
+function closeAbout() {
+    document.getElementById('about').style.transform='';
+    document.getElementById("about").style.transitionDuration="0.5s"
 }

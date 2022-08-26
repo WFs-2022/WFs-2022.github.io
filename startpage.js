5610b8023683558576ae555b87bb6c05516b39cb
W=F*s <106474136+WFs-2022@users.noreply.github.com>
2022-08-26 21:52:37 +0800

    更新 起始页 - 添加cookies

--- startpage.js
+++ startpage.js
@@ -1,4 +1,23 @@
 window.onload=()=>{
+    var arr1 = document.cookie.split('; ');
+    for(var i = 0; i < arr1.length; i++){
+        var arr2 = arr1[i].split('=');
+        if(arr2[0] == 'setsearchEngine') setsearchEngine=arr2[1];
+        else if(arr2[0] == 'setsearchInPage') setsearchInPage=arr2[1];
+        else if(arr2[0] == 'focusInput'){
+            setfocusInput=arr2[1];
+            if(setfocusInput=="0") srchBox.focus();
+        }
+        else if(arr2[0] == 'setclearInputAfterS') setclearInputAfterS=arr2[1]; // 从cookie读取当前模式
+    }
+    searchEngine=setsearchEngine
+    searchInPage=setsearchInPage
+    clearInputAfterS=setclearInputAfterS
+    focusInput=setfocusInput
+    document.getElementById("searchEngines").selectedIndex=setsearchEngine
+    document.getElementById("openInNew").selectedIndex=setsearchInPage
+    document.getElementById("clearInput").selectedIndex=setclearInputAfterS
+    document.getElementById("focusInput").selectedIndex=setfocusInput
     showTime();setInterval(()=>{showTime()},500);
 }
 let timeshow = document.getElementById("time")
@@ -20,15 +39,20 @@ function showTime(){
 let srchBox = document.getElementById("srchBox")
 let setbtn  = document.getElementById("setting")
 let setpage = document.getElementById("settingpage")
-let setsearchEngine=document.getElementById("searchEngines").selectedIndex
 var searchEngines = new Array(
     "https://www.baidu.com/s?wd=",    // 百度
+    "https://cn.bing.com/search?q=",  // 必应
     "https://www.sogou.com/web?query="// 搜狗
 )
 var searchEngine = 0
 var setpagerun = false, setpageon = false
 var pageon = false
-var searching = false, searchInPage = false;
+var searching = false, searchInPage = false
+var clearInputAfterSearch = false, focusInput = true
+let setsearchEngine=document.getElementById("searchEngines").selectedIndex
+let setsearchInPage=document.getElementById("openInNew").selectedIndex
+let setclearInputAfterS=document.getElementById("clearInput").selectedIndex
+let setfocusInput=document.getElementById("focusInput").selectedIndex
 srchBox.onfocus=()=>{
     searching = true;
     document.body.style.animationName="backblur"
@@ -43,6 +67,7 @@ document.onkeydown=function(event){
     if(e && e.keyCode==13 && searching){
         if(!searchInPage) open(searchEngines[searchEngine]+srchBox.value);
         else window.location=(searchEngines[searchEngine]+srchBox.value);
+        if(!clearInputAfterSearch) srchBox.value=""
     }else if(e.keyCode==191){
         e.preventDefault();
         srchBox.focus();
@@ -107,8 +132,29 @@ function closeMoreSet() {
 }
 function saveSearchEngine(){
     setsearchEngine=document.getElementById("searchEngines").selectedIndex
+    setsearchInPage=document.getElementById("openInNew").selectedIndex
+    setclearInputAfterS=document.getElementById("clearInput").selectedIndex
+    setfocusInput=document.getElementById("focusInput").selectedIndex
     if(searchEngine!=setsearchEngine){
         alert("已保存")
         searchEngine=setsearchEngine
+    }if(setsearchInPage!=searchInPage){
+        alert("已保存")
+        searchInPage=setsearchInPage
+    }if(setclearInputAfterS!=clearInputAfterSearch){
+        alert("已保存")
+        clearInputAfterSearch=setclearInputAfterS
+    }if(setclearInputAfterS!=clearInputAfterSearch){
+        alert("已保存")
+        clearInputAfterSearch=setclearInputAfterS
+    }if(setfocusInput!=focusInput){
+        alert("已保存")
+        focusInput=setfocusInput
     }
+    var oDate = new Date();
+    oDate.setDate(oDate.getDate()+365);//访问页面后的365天过期
+    document.cookie="setsearchEngine="+setsearchEngine+"; expires="+oDate.toGMTString()
+    document.cookie="setsearchInPage="+setsearchInPage+"; expires="+oDate.toGMTString()
+    document.cookie="setclearInputAfterS="+setclearInputAfterS+"; expires="+oDate.toGMTString();
+    document.cookie="focusInput="+setfocusInput+"; expires="+oDate.toGMTString();
 }

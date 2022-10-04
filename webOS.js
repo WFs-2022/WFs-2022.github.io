947a120b7d32460b260bb16bebd36e121c784921
W=F*s <106474136+WFs-2022@users.noreply.github.com>
2022-10-04 10:53:46 +0800

    更新 webOS

--- webOS.js
+++ webOS.js
@@ -1,9 +1,13 @@
 window.onload=()=>{
     reloadListeners();
 }
+// 定义
+let barApps = document.getElementById("apps")
 // 新建窗口
-const windowHTML1='<span class="windowCLOSE"><svg viewBox="0 0 32 24"><line x1="10" x2="22" y1="6" y2="18" stroke-width="2" stroke="#121212" stroke-linecap="round" /><line x1="22" x2="10" y1="6" y2="18" stroke-width="2" stroke="#121212" stroke-linecap="round" /></svg></span><span class="windowMINIMIZE"><svg viewBox="0 0 32 24"><line x1="8" x2="24" y1="12" y2="12" stroke-width="2" stroke="#121212" stroke-linecap="round" /></svg></span><span class="windowTOPS"></span><span class="windowFILLINS">'
-const windowHTML2='</span>'
+var windowIDs=0
+const windowHTML1='class="windowCLOSE"><svg viewBox="0 0 32 24"><line x1="10" x2="22" y1="6" y2="18" stroke-width="2" stroke="#121212" stroke-linecap="round" /><line x1="22" x2="10" y1="6" y2="18" stroke-width="2" stroke="#121212" stroke-linecap="round" /></svg></span><span class="windowMAXSIZE"><svg viewBox="0 0 32 24"><line x1="8" x2="24" y1="6" y2="6" stroke-width="2" stroke="#121212" stroke-linecap="round" /><line x1="8" x2="24" y1="18" y2="18" stroke-width="2" stroke="#121212" stroke-linecap="round" /><line x1="8" x2="8" y1="6" y2="18" stroke-width="2" stroke="#121212" stroke-linecap="round" /><line x1="24" x2="24" y1="6" y2="18" stroke-width="2" stroke="#121212" stroke-linecap="round" /></svg></span><span '
+const windowHTML2='class="windowMINIMIZE"><svg viewBox="0 0 32 24"><line x1="8" x2="24" y1="12" y2="12" stroke-width="2" stroke="#121212" stroke-linecap="round" /></svg></span><span class="windowTOPS"></span><span class="windowFILLINS">'
+const windowHTML3='</span>'
 const windowFillinHTML=[
     '',
     '<iframe src="TodoApp.html">',
@@ -17,11 +21,29 @@ const windowFillinHTML=[
     '<iframe src="webOS.html">',
 ]
 function newWindow(wintype) {
+    var newWindowReferHTML = document.createElement("span")
+    newWindowReferHTML.innerHTML='<span class="windowRefers" onclick="minimaxsize(\''+"window"+windowIDs+'\')">'+windowIDs+'</span>'
+    newWindowReferHTML.id="windowRefer"+windowIDs
+    barApps.appendChild(newWindowReferHTML)
     var newWindowHTML = document.createElement("span")
-    newWindowHTML.innerHTML=windowHTML1+windowFillinHTML[wintype]+windowHTML2
+    newWindowHTML.id="window"+windowIDs
+    newWindowHTML.innerHTML="<span onclick='javascript:document.getElementById(\"window"+windowIDs+"\").remove();document.getElementById(\"windowRefer"+windowIDs+"\").remove()'"+windowHTML1+"onclick='javascript:document.getElementById(\"window"+windowIDs+"\").style.transform=\"scale(0)\";document.getElementById(\"window"+windowIDs+"\").style.transitionDuration=\"0.5s\"'"+windowHTML2+windowFillinHTML[wintype]+windowHTML3
+    windowIDs++
     newWindowHTML.className="windows"
     document.body.appendChild(newWindowHTML)
     reloadListeners();
+}
+function minimaxsize(arg){
+    if(document.getElementById(arg).style.transform=='scale(0)'){
+        document.getElementById(arg).style.transform='scale(1)'
+        setTimeout(() => {
+            document.getElementById(arg).style.transitionDuration='0s'
+        }, 500);
+    }
+    else{
+        document.getElementById(arg).style.transitionDuration='0.5s'
+        document.getElementById(arg).style.transform='scale(0)'
+    }
 }
 
 // 窗口移动
@@ -31,22 +53,22 @@ function reloadListeners(){
         const element = windowTOPS[i];
         var windows = element.parentElement;
         const items = windows.children;
-        const closer= items[0], minimizer = items[1]
         element.onmousedown = function (ev) {
             let e = ev || event;
             let x = e.clientX - element.parentElement.offsetLeft; //鼠标点击坐标距离盒子左边缘的距离
             let y = e.clientY - element.parentElement.offsetTop; //鼠标点击坐标距离盒子上边缘的距离
-            document.onmousemove = function (ev) {
+            document.getElementsByTagName("iframe")
+            window.onmousemove = function (ev) {
                 element.parentElement.style.left = ev.clientX - x + 'px';
                 element.parentElement.style.top = ev.clientY - y + 'px';
-                document.onmouseup = ()=>{
-                    document.onmousemove = null;
-                    document.onmouseup = null;
+                window.onmouseup = ()=>{
+                    window.onmousemove = null;
+                    window.onmouseup = null;
                 }
             }
         }
-        closer.onclick=()=>{
-            closer.parentElement.remove();
-        }
+        // closer.onclick=()=>{
+        //     closer.parentElement.remove();
+        // }
     }
 }

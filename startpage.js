df65af68b29cd18968c5320bc157c954fc4d146d
W=F*s <106474136+WFs-2022@users.noreply.github.com>
2022-10-06 20:42:16 +0800

    修复 搜索时传入地址错误

--- startpage.js
+++ startpage.js
@@ -67,8 +67,8 @@ srchBox.onblur=()=>{
 document.onkeydown=function(event){
     var e = event || window.event || arguments.callee.caller.arguments[0];
     if(e && e.keyCode==13 && searching){
-        if(searchInPage=="0") open(searchEngines[searchEngine]+srchBox.value);
-        else window.location=(searchEngines[searchEngine]+srchBox.value);
+        if(searchInPage=="0") open(searchEngines[searchEngine]+encodeURI(srchBox.value));
+        else window.location=(searchEngines[searchEngine]+encodeURI(srchBox.value));
         if(clearInputAfterSearch=="0") srchBox.value=""
     }else if(e.keyCode==191){
         e.preventDefault();
